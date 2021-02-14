1569af902c910f20c9a2253c639d2de4512066ed
DavorJ94 <davorjovanovic94@gmail.com>
2021-02-14 22:11:07 +0100

    added meta data, updated readme, and added tab icon

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This web page contains a list of available APIs, with appropriate links that redirect to the web page of that respective API.
 
-The idea is obtained from Scrimba course. Design and code is my own.
+The idea is obtained from Scrimba course. Design and code is my own. The main idea is to use grid tool in css to create a list of APIs.
 
 Basically, API was used to represent the list of available APIs.
--- index.js
+++ index.js
@@ -30,22 +30,3 @@ function displayAPIs(apis) {
 }
 
 getAPIs().then(displayAPIs);
-
-// getAPIs().then((api) => {
-//   api = api[0];
-//   document.body.innerHTML += `
-// <div class="one-api">
-// <a href="${api.Link}" class="api-link-title-category">
-//   <div class="api-name">${api.API}</div>
-//   <div class="api-category">${api.Category}</div>
-//   </a>
-//   <div class="api-description">${api.Description}</div>
-//   <div class="api-auth">${api.Auth}</div>
-//   <div class="api-https">${
-//     api.https ? "Supports HTTPS" : "Does not support HTTPS"
-//   }</div>
-//   <div class="api-link">${api.Link}</div>
-
-//   </div>
-// `;
-// });
