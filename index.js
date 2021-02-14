18b3f45b8061ec966ea72ee98ba8fdc4c166970f
DavorJ94 <davorjovanovic94@gmail.com>
2021-02-14 21:47:50 +0100

    added title and styling of grid

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+document.body.innerHTML += `<h1 class="title">LIST OF AVAILABLE APIs</h1>`;
+
 async function getAPIs() {
   const response = await fetch("https://api.publicapis.org/entries");
   const data = await response.json();
   return data.entries;
 }
 
-// function displayAPIs(apis) {
-//   apis.map(
-//     (api) =>
-//       (document.body.innerHTML += `
+function displayAPIs(apis) {
+  apis.map(
+    (api) =>
+      (document.body.innerHTML += `
+<div class="one-api">
+<a href="${api.Link}" class="api-link-title-category" target="_blank">
+  <div class="api-name">Name: <span>${api.API}</span></div>
+  <div class="api-category">Category: ${api.Category}</div>
+  </a>
+  <div class="api-description">Description: ${api.Description}</div>
+  <div class="api-auth">Auth: ${api.Auth ? api.Auth : "None"}</div>
+  <div class="api-https">${
+    api.HTTPS ? "Supports HTTPS" : "Does not support HTTPS"
+  }</div>
+  <div class="api-link">Link: <a href="${api.Link}" target="_blank">${
+        api.Link
+      }</a></div>
+
+  </div>
+`)
+  );
+}
+
+getAPIs().then(displayAPIs);
+
+// getAPIs().then((api) => {
+//   api = api[0];
+//   document.body.innerHTML += `
 // <div class="one-api">
 // <a href="${api.Link}" class="api-link-title-category">
 //   <div class="api-name">${api.API}</div>
@@ -21,27 +47,5 @@ async function getAPIs() {
 //   <div class="api-link">${api.Link}</div>
 
 //   </div>
-// `)
-//   );
-// }
-
-// getAPIs().then(displayAPIs);
-
-getAPIs().then((api) => {
-  api = api[0];
-  document.body.innerHTML += `
-<div class="one-api">
-<a href="${api.Link}" class="api-link-title-category">
-  <div class="api-name">${api.API}</div>
-  <div class="api-category">${api.Category}</div>
-  </a>
-  <div class="api-description">${api.Description}</div>
-  <div class="api-auth">${api.Auth}</div>
-  <div class="api-https">${
-    api.https ? "Supports HTTPS" : "Does not support HTTPS"
-  }</div>
-  <div class="api-link">${api.Link}</div>
-
-  </div>
-`;
-});
+// `;
+// });
